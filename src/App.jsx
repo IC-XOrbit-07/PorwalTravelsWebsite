ecf7f9712fc45f3b36cf4ed47a9b4ca0e9a1a192
Indian-Coder <indiancoder92@gmail.com>
2025-01-20 22:48:36 +0530

    0 - Working on navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AnnouncementBanner from "./component/AnnouncementBanner"
+import Navbar from '../src/component/Navbar'
+
+import '../src/Css/Body.css'
+
+function App() {
+  return (
+    <>
+      <AnnouncementBanner/>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/AnnouncementBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Css/AnnouncementBanner.css'
+
+function AnnouncementBanner() {
+  return (
+    <div className='bg-black Announcement_Banner_JS892 text-center'>
+        <a className='hover:cursor-pointer'>Experience seamless journeys with Porwal Travels – unlock quicker getaways with our latest offerings – explore the details!</a>
+    </div>
+  )
+}
+
+export default AnnouncementBanner
