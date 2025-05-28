1466c9d29e5024ea46bd649ebe58379a4bb83309
Indian-Coder <indiancoder92@gmail.com>
2025-05-28 11:05:55 +0530

    Added almost things in rajasthan section + corrected the spelling of official

--- src/component/StateDescription.jsx
+++ src/component/StateDescription.jsx
@@ -3,12 +3,14 @@ import { useParams } from 'react-router-dom'
 import BounceCards from '../Reactbits/BounceCards';
 
 import raj_intro_image from '../Reactbits/RajasthanPhoto/Intro.jpg'
+import EnquirySection from './EnquirySection';
 
 import image1 from '../Reactbits/RajasthanPhoto/1.jpg'
 import image2 from '../Reactbits/RajasthanPhoto/2.jpg'
 import image3 from '../Reactbits/RajasthanPhoto/3.jpg'
 import image4 from '../Reactbits/RajasthanPhoto/4.jpg'
 import image5 from '../Reactbits/RajasthanPhoto/5.jpg'
+import Footer from './Footer';
 
 const images = [image1,image2,image3,image4,image5];
 
@@ -35,7 +37,7 @@ function StateDescription() {
                     Welcome to, {name}
                     <div style={{scale:".9",marginLeft:"-30px"}}>
                         <button className='text-2xl rounded-full' onClick={()=>{}}>
-                            <a href="https://www.tourism.rajasthan.gov.in/" target='_blank' className='font-normal ml-6 md:ml-0'>Offical Website ↗</a>
+                            <a href="https://www.tourism.rajasthan.gov.in/" target='_blank' className='font-normal ml-6 md:ml-0'>Official Website ↗</a>
                         </button>
                         {/* <hr className='relative w-auto mx-2.5 -mt-2' /> */}
                     </div>
@@ -59,6 +61,22 @@ function StateDescription() {
                     />
                 </div>
             </div>
+
+            <div className="flex justify-center rounded-xl">
+                {/* <iframe
+                    className="rounded-xl m-16 md:m-18 xl:m-20 video_show"
+                    src="https://www.youtube.com/embed/6KL2QZ91kAM?controls=0&autohide=1&showinfo=0&rel=0&modestbranding=0&fs=0&iv_load_policy=3&autoplay=1&mute=1&start=0"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                ></iframe> */}
+
+                <iframe className="rounded-xl m-16 md:m-18 xl:m-20 video_show" width="560" height="315" src="https://www.youtube-nocookie.com/embed/UYTYf6MwZj4?si=fJhgxgke4D0sCbUD?controls=0&autohide=1&showinfo=0&rel=0&modestbranding=0&fs=0&iv_load_policy=3&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            </div>
+
+
+            <EnquirySection></EnquirySection>
+            <Footer/>
             
         </>
     )
